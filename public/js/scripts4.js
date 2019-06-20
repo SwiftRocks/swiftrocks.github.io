620a7d5995a9e5ea75dc4171407b64f76408ac12
Bruno Rocha <brunorochaesilva@gmail.com>
2019-06-20 14:02:00 -0300

    Initial Transfer from Firebase

---
+++ public/js/scripts4.js
@@ -0,0 +1,7 @@
+(function ($) {
+    'use strict';
+    jQuery(document).ready(function () {
+        $('pre').addClass("language-swift");
+        $('code').addClass("language-swift");
+    });
+})(jQuery);
