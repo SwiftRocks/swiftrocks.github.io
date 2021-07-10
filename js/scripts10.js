6abf6ec08f4c6893fdd3cda26e28749be8a8e1ee
Bruno Rocha <rochab@spotify.com>
2021-07-10 21:04:28 +0200

    Almost done

--- js/scripts10.js
+++ js/scripts10.js
@@ -2,36 +2,9 @@
     'use strict';
     jQuery(document).ready(function () {
         $('pre').addClass("language-swift");
-        var readHTMLFile = function(url){
-            var toReturn;
-            $.ajax({
-                url: url,
-                async: false
-            }).done(function(data){
-                toReturn = data;
-            });
-            return toReturn;
-        };
-
-       	function getRandomInt(min, max) {
-    		min = Math.ceil(min);
-    		max = Math.floor(max);
-    		return Math.floor(Math.random() * (max - min + 1)) + min;
-		}
-
-        var rss = readHTMLFile("https://swiftrocks.com/rss.xml");
-		var channel = rss.getElementsByTagName("channel")[0];
-
-		var item = channel.getElementsByTagName("item");
-		var rand = getRandomInt(0, item.length);
-
-		var title = item[rand].getElementsByTagName("title")[0].childNodes[0].nodeValue;
-		var link = item[rand].getElementsByTagName("link")[0].childNodes[0].nodeValue;
-
-		document.getElementById("next-article-title").innerHTML = "<a href=\"" + link + "\">" + title + " ‎<i class=\"fa fa-external-link\"></i>‏‏‎</a>"
 
         ///
-        var showSponsor = false;
+        var showSponsor = true;
         ///
         var sponsorTitle = "Title"
         var sponsorIcon = "https://i.imgur.com/tw7D4YJ.jpg"
@@ -42,12 +15,10 @@
         const sponsorDivs = document.getElementsByClassName(sponsorDivId)
 
         for (var i = 0; i<sponsorDivs.length; i++) {
-            if (showSponsor) {
+            if (showSponsor && sponsorDivs[i].contains("hidden")) {
                 sponsorDivs[i].innerHTML = "<a href=\""+sponsorLink+"\" target=\"_blank\"><span></span></a><div class=\"sponsor-article-ad-auto-header\">SPONSOR</div><text><b>"+sponsorTitle+"</b></text><div class=\"sponsor-article-ad-auto-inner\"><img src=\""+sponsorIcon+"\" alt=\"Sponsor icon\"><div class=\"sponsor-article-ad-auto-text\"><p>"+sponsorText+"</p></div></div>"
                 sponsorDivs[i].classList.remove("hidden");
             }
         }
-
-        document.body.classList.remove("hidden");
     });
 })(jQuery);
