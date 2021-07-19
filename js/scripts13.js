8739d4b7a5dec4f19f97022d9aa9d1bd046e2ff7
Bruno Rocha <rochab@spotify.com>
2021-07-19 09:08:36 +0200

    Scripts

--- js/scripts13.js
+++ js/scripts13.js
@@ -2,7 +2,6 @@
     'use strict';
     jQuery(document).ready(function () {
         $('pre').addClass("language-swift");
-
         ///
         var showSponsor = true;
         ///
@@ -15,7 +14,7 @@
         const sponsorDivs = document.getElementsByClassName(sponsorDivId)
 
         for (var i = 0; i<sponsorDivs.length; i++) {
-            if (showSponsor && sponsorDivs[i].contains("hidden")) {
+            if (showSponsor && sponsorDivs[i].classList.contains("hidden")) {
                 sponsorDivs[i].innerHTML = "<a href=\""+sponsorLink+"\" target=\"_blank\"><span></span></a><div class=\"sponsor-article-ad-auto-header\">SPONSOR</div><text><b>"+sponsorTitle+"</b></text><div class=\"sponsor-article-ad-auto-inner\"><img src=\""+sponsorIcon+"\" alt=\"Sponsor icon\"><div class=\"sponsor-article-ad-auto-text\"><p>"+sponsorText+"</p></div></div>"
                 sponsorDivs[i].classList.remove("hidden");
             }
