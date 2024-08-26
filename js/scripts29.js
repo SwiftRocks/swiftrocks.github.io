8c8a90c857b981c5d3d5e68dd39264299b966113
Bruno Rocha <rochab@spotify.com>
2024-08-26 13:47:03 +0200

    JS updates

--- js/scripts29.js
+++ js/scripts29.js
@@ -4,11 +4,21 @@
         $('pre').addClass("language-swift");
         ///
         var showSponsor = true;
+        var sponsorData = [
+            ["Burnout Buddy - App Blocker for iOS", "https://i.imgur.com/OHTdc17.png", "Modern apps like social media hook you at a psychological level. Burnout Buddy helps you break free from addictive apps and heal their impact on your mental health. Try it and see the benefits for yourself; it's free and requires no account.", "https://burnoutbuddy.io"],
+            ["Emerge Tools - Build better mobile apps", "https://i.imgur.com/F4ZGrLz.png", "Trusted by top mobile teams like Duolingo, Square & DoorDash to make the best apps possible. Start using Emerge Tools today!", "https://www.emergetools.com/?utm_source=swiftrocks&utm_medium=sponsor&utm_campaign=emerge"],
+            ["Emerge Tools Blog - Sharing our journey building the future of mobile development", "https://i.imgur.com/F4ZGrLz.png", "The Emerge blog is dedicated to exploring the latest advancements and trends in mobile app performance. From the newest developments in mobile technology to cutting-edge optimization strategies, we cover it all.", "https://www.emergetools.com/blog?utm_source=swiftrocks&utm_medium=sponsor&utm_campaign=emerge"],
+        ]
+        var activeSponsors = [0]
         ///
-        var sponsorTitle = "Burnout Buddy - App Blocker for iOS"
-        var sponsorIcon = "https://i.imgur.com/OHTdc17.png"
-        var sponsorText = "Modern apps like social media hook you at a psychological level. Burnout Buddy helps you break free from addictive apps and heal their impact on your mental health. Try it and see the benefits for yourself; it's free and requires no account."
-        var sponsorLink = "https://burnoutbuddy.io"
+
+        var pickedIndex = activeSponsors[Math.floor(Math.random() * activeSponsors.length)]
+        var sponsor = sponsorData[activeSponsors[Math.floor(Math.random() * activeSponsors.length)]]
+
+        var sponsorTitle = sponsor[0]
+        var sponsorIcon = sponsor[1]
+        var sponsorText = sponsor[2]
+        var sponsorLink = sponsor[3]
 
         const sponsorDivId = "sponsor-article-ad-auto"
         const sponsorDivs = document.getElementsByClassName(sponsorDivId)
